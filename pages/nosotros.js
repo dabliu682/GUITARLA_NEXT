e44f89119ed2d379a1b0a31cf5b8f09a756fc6a1
dabliu682 <dabliu.682@gmail.com>
2022-05-27 22:20:44 -0500

    Creacion de header

---
+++ pages/nosotros.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout"
+
+const Nosotros = () => {
+  return (
+    <div>
+        <Layout
+            pagina='Inicio'
+        >
+        <h1>Desde nosotros</h1>
+        </Layout>
+    </div>
+  )
+}
+
+export default Nosotros
